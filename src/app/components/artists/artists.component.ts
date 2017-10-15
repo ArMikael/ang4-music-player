edf08ef00476ca69644372ef991b8a54d87e7289
Michael Treser <armikael2@gmail.com>
2017-10-16 00:19:02 +0300

    Added ngForm checkbox and select.

--- src/app/components/artists/artists.component.ts
+++ src/app/components/artists/artists.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artists.component.scss']
 })
 export class ArtistsComponent implements OnInit {
+  countries = [
+    { id: 1, name: 'USA' },
+    { id: 2, name: 'Israel' },
+    { id: 3, name: 'Russia' },
+    { id: 4, name: 'Italy' }
+  ];
 
   constructor() { }
 
